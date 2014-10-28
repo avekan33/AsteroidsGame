d80b414999affcc94a69ee6c8eb01fded0d5842b
unknown <avkandala@697-334-Valmar.sf.k12.internal>
2014-10-28 11:00:17 -0700

    Asteroids Rev. 1

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip ship;
 Star[] yes;
 int width, height;
@@ -42,15 +58,16 @@ public void keyPressed()
   }
   if(keyCode == UP)
   {
-    ship.accelerate(.2);
+    ship.accelerate(.2f);
   }
   else if(keyCode == DOWN)
   {
-    ship.accelerate(-.2);
+    ship.accelerate(-.2f);
   }
   if(key == 'h')
   {
     hyperspace(ship, yes);
+    println('h');
   }
 }
 public void hyperspace(SpaceShip dog, Star[] ye)
@@ -205,3 +222,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
